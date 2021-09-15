1ba08a22f9d0ee8b263c175a765b5822d7abb9f9
dharini231 <90209992+dharini231@users.noreply.github.com>
2021-09-15 20:06:50 +0800

    Add files via upload

---
+++ tickectbooking.java
@@ -0,0 +1,8 @@
+class tickectbooking{
+	public static void main(String args[]){
+	byte Noofseatsavailable = 1;
+	if(Noofseatsavailable>=1){		
+	System.out.println(" tickectbooking is done");
+	}
+	}
+}
